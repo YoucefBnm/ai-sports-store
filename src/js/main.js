b6921015de9c66b4798aab03c852bfd9fa9a930a
Josef Bnm <ssefbnm@gmail.com>
2022-08-23 00:11:01 +0100

    add mobile breakpoint

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,23 @@
 const swiper = new Swiper(".mySwiper", {
     slidesPerView: 3.3,
     spaceBetween: 20,
-    
+    breakpoints: {
+      
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1.2,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 3.3,
+        spaceBetween: 20,
+      }
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -12,4 +28,25 @@ const swiper = new Swiper(".mySwiper", {
       },
   });
 
-  console.log('test')
+  const swiperGallery = new Swiper(".mySwiper-gallery", {
+    
+    breakpoints: {
+      
+      320: {
+        slidesPerView: 1.2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1.2,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      640: {
+        slidesPerView: 1.3,
+        spaceBetween: 20,
+      }
+    },
+    
+    
+  });
